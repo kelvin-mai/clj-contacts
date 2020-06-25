3880f0dfb63d57ae91e8e2a5fcbe73064a18e243
Kelvin Mai <kelvin.mai002@gmail.com>
2020-06-24 19:09:24 -0700

    set up server

--- src/clj/contacts/core.clj
+++ src/clj/contacts/core.clj
@@ -1,4 +1,28 @@
-(ns contacts.core)
+(ns contacts.core
+  (:require [org.httpkit.server :refer [run-server]]
+            [reitit.ring :as ring]))
+
+(defonce server (atom nil))
+
+(def app
+  (ring/ring-handler
+   (ring/router
+    [["/api"
+      ["/ping" {:name ::ping
+                :get (fn [_] {:status 200
+                              :body "ok"})}]]])))
+
+(defn stop-server []
+  (when-not (nil? @server)
+    (@server :timeout 100)
+    (reset! server nil)))
 
 (defn -main []
-  (println "Hello world"))
+  (println "Server started")
+  (reset! server (run-server app {:port 4000})))
+
+(comment
+  @server
+  (-main)
+  (stop-server)
+  ())
