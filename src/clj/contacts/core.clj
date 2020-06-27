89e220884cb1dc76212bc034bf19390b1389f951
Kelvin Mai <kelvin.mai002@gmail.com>
2020-06-26 18:58:44 -0700

    middleware negiotiation

--- src/clj/contacts/core.clj
+++ src/clj/contacts/core.clj
@@ -1,16 +1,33 @@
 (ns contacts.core
   (:require [org.httpkit.server :refer [run-server]]
-            [reitit.ring :as ring]))
+            [reitit.ring :as ring]
+            [reitit.ring.middleware.exception :refer [exception-middleware]]
+            [reitit.ring.middleware.muuntaja :refer [format-negotiate-middleware
+                                                     format-request-middleware
+                                                     format-response-middleware]]
+            [muuntaja.core :as m]
+            [clojure.pprint :refer [pprint]]))
 
 (defonce server (atom nil))
 
 (def app
   (ring/ring-handler
    (ring/router
-    [["/api"
+    [["/" {:get (fn [req]
+                  (pprint req)
+                  {:status 200
+                   :body "it works!"})}]
+     ["/api"
       ["/ping" {:name ::ping
-                :get (fn [_] {:status 200
-                              :body "ok"})}]]])))
+                :get (fn [{:keys [body-params]}]
+                       (pprint body-params)
+                       {:status 200
+                        :body {:hello "world"}})}]]]
+    {:data {:muuntaja m/instance
+            :middleware [format-negotiate-middleware
+                         format-response-middleware
+                         exception-middleware
+                         format-request-middleware]}})))
 
 (defn stop-server []
   (when-not (nil? @server)
@@ -21,8 +38,10 @@
   (println "Server started")
   (reset! server (run-server app {:port 4000})))
 
-(comment
-  @server
-  (-main)
+(defn restart-server []
   (stop-server)
-  ())
+  (-main))
+
+(comment
+  (stop-server)
+  (restart-server))
