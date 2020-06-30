4d038415f0a52ee1f71d1405dc9332de93232747
Kelvin Mai <kelvin.mai002@gmail.com>
2020-06-30 10:32:39 -0700

    set up hx and tailwind for frontend

--- src/cljs/contacts/core.cljs
+++ src/cljs/contacts/core.cljs
@@ -1,11 +1,30 @@
 (ns contacts.core
-  (:require [ajax.core :refer [GET]]))
+  (:require [ajax.core :refer [GET]]
+            [hx.react :as hx :refer [defnc]]
+            [hx.hooks :as hooks]
+            ["react-dom" :as dom]))
 
-(defn ^:export init []
-  (println "Hello world"))
+(defnc greeting
+  [{:keys [name]}]
+  [:div "Hello, "
+   [:strong name] "!"])
+
+(defnc app []
+  (let [[state set-state] (hooks/useState {:name "Helix User"})]
+    (prn state)
+    [:div
+     [:h1 "Welcome!"]
+     [greeting {:name (:name state)}]
+     [:input {:class '[border shadow]
+              :value (:name state)
+              :on-change #(set-state assoc :name (.. % -target -value))}]]))
+
+(defn ^:export ^:after-load init []
+  (dom/render (hx/f [app]) (js/document.getElementById "app")))
 
 (comment
   (GET "http://localhost:4000/api/contacts"
     {:handler (fn [response]
                 (.log js/console response))})
+  (init)
   ())
